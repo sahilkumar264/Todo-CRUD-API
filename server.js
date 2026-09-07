f706967fde6af738ecb8c599178c385d459eeb25
Sahil <sahilkumarkavita27@gmail.com>
2026-09-07 12:18:12 +0530

    Stage 0: hello server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Todo-CRUD-API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
